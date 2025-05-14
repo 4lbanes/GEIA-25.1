d6a5b876da723147bfe06832f4fe8ffe0dfb6372
kiyohiroleo <kiyohiroleo@gmail.com>
2025-05-14 20:33:05 -0300

    feat: desvio e variancia, moda falta cond bimodal

--- submissions/etr2/Análise.py
+++ submissions/etr2/Análise.py
@@ -22,19 +22,22 @@ def mean(array):
     return np.mean(array)
 
 def mode(array):
-    pass
+    valores, counts = np.unique(array, return_counts=True)
+    max = np.argmax(counts)
+    return valores[max]
+    # falta fazer o bimodal da silva
     
 def median(array):
     return np.median(array)
 
 def std(array):
-    pass
+    return np.std(array)
 
 def interval(array):
     pass
 
 def variance(array):
-    pass
+    return np.var(array)
 
 def z_score(array):
     pass
@@ -43,11 +46,25 @@ def cerca_turkey(array):
     pass
 
 def generate_report():
+
     print("A média: ")
     for nome, variavel in variaveis.items():
         print(f"{nome}, {mean(variavel)}")
+
     print("A mediana: ")
     for nome, variavel in variaveis.items():
         print(f"{nome}, {median(variavel)}")
+        
+    print("A moda: ")
+    for nome, variavel in variaveis.items():
+        print(f"{nome}, {mode(variavel)}")
+
+    print("O desvio padrão: ")
+    for nome, variavel in variaveis.items():
+        print(f"{nome}, {std(variavel):.2f}")
+    
+    print("A variância: ")
+    for nome, variavel in variaveis.items():
+        print(f"{nome}, {variance(variavel):.2f}")
 
 generate_report()
