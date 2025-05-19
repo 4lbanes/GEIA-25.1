b2a301653c4e25540ff6df12d66cdd7d1d86ff1d
Paiva91 <darciohenripaiva@gmail.com>
2025-05-19 19:37:44 -0300

    test: Escrevendo o rascunho dos metodos e das classes

--- submissions/Entregavel 2/import.py
+++ submissions/Entregavel 2/import.py
@@ -1,3 +1,8 @@
 class ImportarDados():
-    def __init__(self):
-        pass
+    def __init__(self, caminho_arquivo):
+        self.caminho = caminho_arquivo
+        self.dados = None
+
+    def importar(self):
+        self.dados = pd.read_csv(self.caminho)
+        return self.dados
