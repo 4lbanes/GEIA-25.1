e23958a06043770738e0a864daa6c51665c373ff
Paiva91 <darciohenripaiva@gmail.com>
2025-05-19 19:38:07 -0300

    test: Escrevendo o rascunho dos metodos e das classes

--- submissions/Entregavel 2/analise.py
+++ submissions/Entregavel 2/analise.py
@@ -1,22 +1,33 @@
+import numpy as np
+
 class AnaliseEstatistica():
     def __init__(self):
         pass
 
-    def media():
+    def mean(array):
+        mean = np.mean(array)
+        return mean 
+    def mode(array):
         pass
-    def mode():
-        pass
-    def median():
-        pass
-    def std():
-        pass
-    def interval():
-        pass
-    def variance():
-        pass
-    def zscore():
-        pass
-    def cerca_tukey():
-        pass
+    def median(array):
+        median = np.median(array)
+        return median
+    def std(array):
+        std = np.std(array)
+        return std 
+    def interval(array):
+        return np.max(array) - np.min(array)
+    def variance(array):
+        variance = np.var(array)
+        return variance
+    def zscore(array):
+        return (array - np.mean(array)) / np.std(array)
+    def cerca_tukey(array):
+        q1 = np.percentile(array, 25)
+        q3 = np.percentile(array, 75)
+        iqr = q3 - q1
+        limite_inferior = q1 - 1.5 * iqr
+        limite_superior = q3 + 1.5 * iqr
+        return limite_inferior, limite_superior
     def generate_report():
         pass
--- submissions/Entregavel 2/import.py
+++ submissions/Entregavel 2/import.py
@@ -1,8 +1,10 @@
+import numpy as np
+
 class ImportarDados():
     def __init__(self, caminho_arquivo):
         self.caminho = caminho_arquivo
         self.dados = None
 
     def importar(self):
-        self.dados = pd.read_csv(self.caminho)
+        self.dados = np.loadtxt(self.caminho, delimiter=',', skiprows=1)
         return self.dados
